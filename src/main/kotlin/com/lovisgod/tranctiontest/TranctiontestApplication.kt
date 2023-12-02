3304f6f28a0e0f419da6e9e4190a92f56e5581aa
lovisgod <ayooluwa.olosunde@gmail.com>
2023-12-02 10:37:03 +0100

    feature: used Q2 and also Qserver to handle more connections and asynchronous request handling

--- src/main/kotlin/com/lovisgod/tranctiontest/TranctiontestApplication.kt
+++ src/main/kotlin/com/lovisgod/tranctiontest/TranctiontestApplication.kt
@@ -1,10 +1,18 @@
 package com.lovisgod.tranctiontest
 
+import org.jpos.q2.Q2
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TranctiontestApplication
+class TranctiontestApplication: CommandLineRunner {
+	override fun run(vararg args: String?) {
+		val q2 = Q2(args)
+		q2.start()
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<TranctiontestApplication>(*args)
--- src/main/kotlin/com/lovisgod/tranctiontest/services/MessageRecieverService.kt
+++ src/main/kotlin/com/lovisgod/tranctiontest/services/MessageRecieverService.kt
@@ -7,6 +7,7 @@ import org.jpos.iso.ISOServer
 import org.jpos.iso.ServerChannel
 import org.jpos.iso.channel.ASCIIChannel
 import org.jpos.iso.packager.GenericPackager
+import org.jpos.q2.Q2
 import org.jpos.util.LogSource
 import org.jpos.util.Logger
 import org.jpos.util.SimpleLogListener
@@ -21,18 +22,26 @@ class MessageReceiverService {
 
     @PostConstruct
     fun initializeIsoServer() {
-        val logger = Logger()
-        val hostname = Constants.HOST_NAME
-        val port = Constants.HOST_PORT
-        var data = javaClass.getResourceAsStream("/fields.xml")
-        val packager = GenericPackager(data)
-        logger.addListener(SimpleLogListener(System.out))
-        val channel: ServerChannel = ASCIIChannel(hostname, port, packager)
-        (channel as LogSource).setLogger(logger, "channel")
-        val server = ISOServer(port, channel, null)
-        server.setLogger (logger, "server")
-        server.addISORequestListener(requestListener)
-        Thread (server).start ()
+//        val logger = Logger()
+//        val hostname = Constants.HOST_NAME
+//        val port = Constants.HOST_PORT
+//        var data = javaClass.getResourceAsStream("/fields.xml")
+//        val packager = GenericPackager(data)
+//        logger.addListener(SimpleLogListener(System.out))
+//        val channel: ServerChannel = ASCIIChannel(hostname, port, packager)
+//        (channel as LogSource).setLogger(logger, "channel")
+//        val server = ISOServer(port, channel, null)
+//        server.setLogger (logger, "server")
+//        server.addISORequestListener(requestListener)
+//        Thread (server).start ()
+
+        // Decided to Q server for the purpose of being able to accept multiple connections
+//        try {
+//            val q2 = Q2()
+//            Thread(q2).start()
+//        } catch (e: Exception) {
+//            println(e)
+//        }
     }
 
 }
